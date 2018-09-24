5966a25daeb440be2c58ff130c9eb5aec800e213
RRoja <43461278+RRoja@users.noreply.github.com>
2018-09-24 15:26:34 +0530

    Delete VersionPage.java

--- KTOC_TRB_TestMethods/src/com/KTOC/TRB/testautomation/ObjectRepository/VersionPage.java
+++
@@ -1,9 +0,0 @@
-package com.KTOC.TRB.testautomation.ObjectRepository;
-
-public class VersionPage 
-{
-	public static String ReleaseVersionWindow = "//div[@data-ctcwgtname='SubFormVersionSelector']";
-	
-	public static String ReleaseOkButton = "//button[@data-ctcname ='KNB_Selector_Ok']/div";
-	//public static String ReleaseNo
-}
